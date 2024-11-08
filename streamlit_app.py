c8dbefb7854845b8a5883a490ffb8851a0b6873b
leejihoooo <20242721@sdhs.gwe.hs.kr>
2024-11-08 09:19:07 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,10 +2,10 @@ import streamlit as st
 import pandas as pd
 
 # 타이틀 설정
-st.title("Streamlit 기본 예제")
+st.title("fs")
 
 # 서브타이틀
-st.subheader("기본적인 표와 HTML 메시지 생성")
+st.subheader("good")
 
 # 표 데이터 생성
 data = {
